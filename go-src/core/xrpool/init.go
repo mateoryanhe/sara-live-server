1b5e7b071ccf4337798aa17903d66ab97de659df
HDdante3 <candanteboz@hotmail.com>
2026-06-25 12:19:33 +0800

    日志fix

--- go-src/core/xrpool/init.go
+++ go-src/core/xrpool/init.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/grpool"
-	"github.com/gogf/gf/v2/util/gutil"
 	"xr-game-server/constants/common"
 	"xr-game-server/core/cfg"
 )
@@ -21,14 +20,8 @@ func Init() {
 }
 
 // GetGoPool 防止go 过多,消耗内存
-
 func AddWithRecover(ctx context.Context, userFunc grpool.Func) {
 	pool.AddWithRecover(ctx, userFunc, func(ctx context.Context, exception error) {
-
-		gutil.TryCatch(ctx, func(try context.Context) {
-			userFunc(ctx)
-		}, func(catch context.Context, exception error) {
-			g.Log().Errorf(catch, "协程池发生错误 AddWithRecover error %v", gerror.Stack(exception))
-		})
+		g.Log().Errorf(ctx, "协程池发生错误 AddWithRecover error %v", gerror.Stack(exception))
 	})
 }
